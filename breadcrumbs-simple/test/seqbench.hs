e9c6c77dc63ec40adf598f100e1d6ed0a2927ebe
lyxia <lysxia@gmail.com>
2016-11-11 22:08:43 +0000

    Add sequential benchmark

---
+++ breadcrumbs-simple/test/seqbench.hs
@@ -0,0 +1,22 @@
+import Criterion.Main
+import Data.Foldable
+
+import qualified Data.Breadcrumbs as B
+import qualified Data.Breadcrumbs.DropList as D
+import qualified Data.Breadcrumbs.Deque as Q
+import qualified Data.Breadcrumbs.Seq as S
+import qualified Data.Breadcrumbs.Flips as F
+import Data.Breadcrumbs.Test
+
+main = defaultMain
+  [ bench "TreeBuffer" $ seqbench B.empty B.push `whnf` length
+  , bench "DropList" $ seqbench D.empty D.push `whnf` D.len
+  , bench "Deque" $ seqbench Q.empty Q.push `whnf` Q.len
+  , bench "Seq" $ seqbench S.empty S.push `whnf` S.len
+  , bench "Flips" $ seqbench F.empty F.push `whnf` F.len
+  ]
+
+size = 10
+iters = 1000
+
+seqbench = appendStuff size iters
